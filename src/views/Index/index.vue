745631c86c52930404eb62f0d287fa553b0fde40
lanpang-zi <985412513@qq.com>
2019-06-11 23:59:54 +0800

    个人主页

--- src/views/Index/index.vue
+++ src/views/Index/index.vue
@@ -1,12 +1,12 @@
 <template>
    <div>
      <router-view></router-view>
-       <h1>正在热映</h1>
+       
 
-     <van-tabbar v-model="active">
-     <van-tabbar-item icon="home-o">热映</van-tabbar-item>
-     <van-tabbar-item icon="search" dot>影院</van-tabbar-item>
-     <van-tabbar-item icon="friends-o" info="5">我的</van-tabbar-item>
+     <van-tabbar v-model="active" >
+     <van-tabbar-item icon="hot" to="/films">热映</van-tabbar-item>
+     <van-tabbar-item icon="video" dot to="/cinemas">影院</van-tabbar-item>
+     <van-tabbar-item icon="friends-o" info="5" to="/center">我的</van-tabbar-item>
     
      </van-tabbar>
    </div>
@@ -19,7 +19,12 @@ export default {
     return {
       active: 0
     }
+  },
+  methods: {
+    
+
   }
 }
 </script>
+
 
