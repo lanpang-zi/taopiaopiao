f7a4ad5c30698b592f21ad87ca12f08b04a3d063
nanxiwen <1215461803@qq.com>
2019-06-14 12:10:06 +0800

    影院静态初始化

---
+++ src/components/Topbar.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="topbar">
+    <div class="city-selected">
+      <span>北京</span>
+      <i>></i>
+    </div>
+    <div class="topbar-wrap">
+      <div class="topbar-item left">
+        <span>全城</span>
+        <i></i>
+      </div>
+      <div class="topbar-item left">
+        <span>综合排序</span>
+        <i></i>
+      </div><div class="topbar-item left">
+        <span>特色</span>
+        <i></i>
+      </div>
+      <div class="topbar-item left search-selector">
+        <span>搜索</span>
+        <i></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Topbar',
+
+  props: {
+    tops: Array
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.topbar{
+  display: flex;
+  width: 100%;
+  color: #999;
+  box-sizing: border-box;
+  z-index: 99;
+  background-color: #fff;
+  position: fixed;
+  top: 0;
+  border-bottom: 1px solid #ccc;
+  .city-selected{
+    width: 105px;
+    text-align: center;
+    line-height: 42px;
+    span{      
+    }
+  }
+  .topbar-wrap{
+    flex: 1;
+    .left{
+      float: left;
+    }
+    .topbar-item{
+      line-height: 42px;
+      width: 75px
+    }
+    .search-selector{
+      width: 42px;
+    }
+  }
+}
+</style>
--- src/views/Index/cinema.vue
+++ src/views/Index/cinema.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>影院</div>
+  <div class="bd">
+    <Topbar></Topbar>
+    <Cinemalist></Cinemalist>
+  </div>
 </template>
+
+<script>
+import Topbar from '@/components/Topbar.vue'
+import Cinemalist from '@/components/Cinemalist.vue'
+export default {
+  components: {
+    Topbar: Topbar,
+    Cinemalist: Cinemalist
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.bd{
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 50px;
+  width: 100%;
+}
+</style>
