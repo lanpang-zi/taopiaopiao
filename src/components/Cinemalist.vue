d3af284c5e9e0dcc0218c2058c991010778c0991
lanpang-zi <985412513@qq.com>
2019-06-14 17:50:12 +0800

    ccc

---
+++ src/components/Cinemalist.vue
@@ -0,0 +1,422 @@
+<template>
+  <div class="tab-bd">
+    <ul>
+      <li>
+        <div class="list-item-container">
+          <div class="list-title-warper">
+            <h3 class="list-title-left">
+              <span class="list-title">北京百老汇电影中心当代MOMA店</span>
+            </h3>
+            <span class="list-price">28.8元<span>起</span></span>
+          </div>
+          <div class="list-address">
+            <span class="list-distance"></span>
+            <div class="list-location">东城区香河园路1号当代MOMA北区T4座</div>
+            <div class="list-search-distance"></div>
+          </div>
+          <div class="list-status">
+            <div class="tpp-lbl tpp-lbl-outline-info">观影小食</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">艺术影厅</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">可停车</div>
+          </div>
+          <ul class="list-activity">
+            <li class="activity-item">
+              <span class="icon-activity icon-0">新</span>
+              新人专享
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <div class="list-item-container">
+          <div class="list-title-warper">
+            <h3 class="list-title-left">
+              <span class="list-title">中影国际影城北京昌平永旺店</span>
+            </h3>
+            <span class="list-price">19.9元<span>起</span></span>
+          </div>
+          <div class="list-address">
+            <span class="list-distance"></span>
+            <div class="list-location">昌平区北清路1号永旺国际商城3楼（生命科学园站下车即到）</div>
+            <div class="list-search-distance"></div>
+          </div>
+          <div class="list-status">
+            <div class="tpp-lbl tpp-lbl-outline-info">观影小食</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">艺术影厅</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">可停车</div>
+          </div>
+          <ul class="list-activity">
+            <li class="activity-item">
+              <span class="icon-activity icon-0">新</span>
+              新人专享
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <div class="list-item-container">
+          <div class="list-title-warper">
+            <h3 class="list-title-left">
+              <span class="list-title">保利国际影城-北京万源店</span>
+            </h3>
+            <span class="list-price">19.9元<span>起</span></span>
+          </div>
+          <div class="list-address">
+            <span class="list-distance"></span>
+            <div class="list-location">丰台区东高地万源北路航天万源广场五楼</div>
+            <div class="list-search-distance"></div>
+          </div>
+          <div class="list-status">
+            <div class="tpp-lbl tpp-lbl-outline-info">观影小食</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">艺术影厅</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">可停车</div>
+          </div>
+          <ul class="list-activity">
+            <li class="activity-item">
+              <span class="icon-activity icon-0">新</span>
+              新人专享
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <div class="list-item-container">
+          <div class="list-title-warper">
+            <h3 class="list-title-left">
+              <span class="list-title">北京搜秀影城</span>
+            </h3>
+            <span class="list-price">19.9元<span>起</span></span>
+          </div>
+          <div class="list-address">
+            <span class="list-distance"></span>
+            <div class="list-location">东城区崇文门外大街40号搜秀商城9层北京搜秀影院</div>
+            <div class="list-search-distance"></div>
+          </div>
+          <div class="list-status">
+            <div class="tpp-lbl tpp-lbl-outline-info">观影小食</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">艺术影厅</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">可停车</div>
+          </div>
+          <ul class="list-activity">
+            <li class="activity-item">
+              <span class="icon-activity icon-0">新</span>
+              新人专享
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <div class="list-item-container">
+          <div class="list-title-warper">
+            <h3 class="list-title-left">
+              <span class="list-title">中影国际影城小西天中国巨幕头等舱</span>
+            </h3>
+            <span class="list-price">19.9元<span>起</span></span>
+          </div>
+          <div class="list-address">
+            <span class="list-distance"></span>
+            <div class="list-location">西城区新街口外大街25号</div>
+            <div class="list-search-distance"></div>
+          </div>
+          <div class="list-status">
+            <div class="tpp-lbl tpp-lbl-outline-info">观影小食</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">艺术影厅</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">可停车</div>
+          </div>
+          <ul class="list-activity">
+            <li class="activity-item">
+              <span class="icon-activity icon-0">新</span>
+              新人专享
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <div class="list-item-container">
+          <div class="list-title-warper">
+            <h3 class="list-title-left">
+              <span class="list-title">UME影城（北京华星店）</span>
+            </h3>
+            <span class="list-price">34.4元<span>起</span></span>
+          </div>
+          <div class="list-address">
+            <span class="list-distance"></span>
+            <div class="list-location">海淀区双榆树科学院南路44号（双安商场对面）</div>
+            <div class="list-search-distance"></div>
+          </div>
+          <div class="list-status">
+            <div class="tpp-lbl tpp-lbl-outline-info">观影小食</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">艺术影厅</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">可停车</div>
+          </div>
+          <ul class="list-activity">
+            <li class="activity-item">
+              <span class="icon-activity icon-0">新</span>
+              新人专享
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <div class="list-item-container">
+          <div class="list-title-warper">
+            <h3 class="list-title-left">
+              <span class="list-title">北京百老汇电影中心当代MOMA店</span>
+            </h3>
+            <span class="list-price">28.8元<span>起</span></span>
+          </div>
+          <div class="list-address">
+            <span class="list-distance"></span>
+            <div class="list-location">东城区香河园路1号当代MOMA北区T4座</div>
+            <div class="list-search-distance"></div>
+          </div>
+          <div class="list-status">
+            <div class="tpp-lbl tpp-lbl-outline-info">观影小食</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">艺术影厅</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">可停车</div>
+          </div>
+          <ul class="list-activity">
+            <li class="activity-item">
+              <span class="icon-activity icon-0">新</span>
+              新人专享
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <div class="list-item-container">
+          <div class="list-title-warper">
+            <h3 class="list-title-left">
+              <span class="list-title">北京百老汇电影中心当代MOMA店</span>
+            </h3>
+            <span class="list-price">28.8元<span>起</span></span>
+          </div>
+          <div class="list-address">
+            <span class="list-distance"></span>
+            <div class="list-location">东城区香河园路1号当代MOMA北区T4座</div>
+            <div class="list-search-distance"></div>
+          </div>
+          <div class="list-status">
+            <div class="tpp-lbl tpp-lbl-outline-info">观影小食</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">艺术影厅</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">可停车</div>
+          </div>
+          <ul class="list-activity">
+            <li class="activity-item">
+              <span class="icon-activity icon-0">新</span>
+              新人专享
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <div class="list-item-container">
+          <div class="list-title-warper">
+            <h3 class="list-title-left">
+              <span class="list-title">北京百老汇电影中心当代MOMA店</span>
+            </h3>
+            <span class="list-price">28.8元<span>起</span></span>
+          </div>
+          <div class="list-address">
+            <span class="list-distance"></span>
+            <div class="list-location">东城区香河园路1号当代MOMA北区T4座</div>
+            <div class="list-search-distance"></div>
+          </div>
+          <div class="list-status">
+            <div class="tpp-lbl tpp-lbl-outline-info">观影小食</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">艺术影厅</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">可停车</div>
+          </div>
+          <ul class="list-activity">
+            <li class="activity-item">
+              <span class="icon-activity icon-0">新</span>
+              新人专享
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <div class="list-item-container">
+          <div class="list-title-warper">
+            <h3 class="list-title-left">
+              <span class="list-title">北京百老汇电影中心当代MOMA店</span>
+            </h3>
+            <span class="list-price">28.8元<span>起</span></span>
+          </div>
+          <div class="list-address">
+            <span class="list-distance"></span>
+            <div class="list-location">东城区香河园路1号当代MOMA北区T4座</div>
+            <div class="list-search-distance"></div>
+          </div>
+          <div class="list-status">
+            <div class="tpp-lbl tpp-lbl-outline-info">观影小食</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">艺术影厅</div>
+            <div class="tpp-lbl tpp-lbl-outline-info">可停车</div>
+          </div>
+          <ul class="list-activity">
+            <li class="activity-item">
+              <span class="icon-activity icon-0">新</span>
+              新人专享
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cinemalist',
+
+  props: {
+    cinemas: Array
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.tab-bd{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 43px;
+  left: 0;
+  right: 0;
+  overflow-y: auto;
+  li{
+    font-size: 12px;
+    line-height: 200%;
+    padding: 0 15px;
+    padding-bottom: 1px;
+    overflow: hidden;
+    .list-item-container{
+      // background: transparent linear-gradient(0deg,#ededed,#ededed 50%,transparent 0,transparent 0)0 100% repeat-x;
+      // background-size: 100% 1px;
+      padding-bottom: 12px;
+      display: block;
+      position: relative;
+      padding-top: 15px;
+      .list-title-warper{
+        display: flex;
+        font-size: 14px;
+        line-height: 24px;
+        margin-bottom: 2px;
+        font-weight: 500;
+        color: #000;
+        .list-title-left{
+          display: flex;
+          flex: 1;
+          width: 0;
+          .list-title{
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            vertical-align: top;
+            margin-right: 10px;
+          }
+        }
+      .list-price{
+        line-height: 24px;
+        vertical-align: baseline;
+        color: #ff4d64;
+        margin-right: 1.125px;
+        font-size: 15px;
+        font-weight: 400;
+        padding-left: 15px;
+        span{
+          font-size: .625rem;
+          color: #999;
+        }
+      }
+      }
+      .list-address{
+        position: relative;
+        line-height: 18px;
+        padding-right: 15px;
+        .list-distance{
+          position: absolute;
+          right: 0;
+          top: 0;
+          font-size: 13px;
+          padding-right: 15px;
+          color: #999;
+        }
+        .list-location{
+          width: 270px;
+          color: #666;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          font-size: 13px;
+        }
+        .list-search-distance{
+          line-height: 28.125px;
+          display: flex;
+          align-items: center;
+          color: #999;
+          font-size: 12px;
+        }
+      }
+    }
+    .list-activity .activity-item{
+      height: 15px;
+      line-height: 15px;
+      color: #999;
+      font-size: 12px;
+      margin-top: 4.875px;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+      .icon-activity {
+        border-radius: 2px;
+        display: inline-block;
+        width: 15px;
+        height: 15px;
+        line-height: 15px;
+        text-align: center;
+        color: #fff;
+        font-size: 12px;
+        margin-right: 3px;
+      }
+      .icon-0 {
+        background-color: #fe7a4c;
+      }
+    }
+  }
+}
+.tpp-lbl{
+  display: inline-block;
+  text-align: center;
+  user-select: none;
+  color: #fff;
+  background-color: #999;
+  position: relative;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  white-space: nowrap;
+  height: 16px;
+  line-height: 16px;
+  padding: 0 4px;
+  border-radius: 2px;
+}
+.tpp-lbl-outline-info {
+  color: #349cec;
+  background-color: transparent;
+  position: relative;
+}
+.tpp-lbl-outline-info:after {
+  position: absolute;
+  content: "";
+  top: 0;
+  left: 0;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  border: 1px solid #349cec;
+  border-radius: 4px;
+  pointer-events: none;
+  transform-origin: 0 0;
+}
+</style>
